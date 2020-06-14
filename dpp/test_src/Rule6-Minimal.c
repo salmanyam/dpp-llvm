50cefcae52dbaa7d13861ad02f399459e784b2a2
Hans Liljestrand <hans@liljestrand.dev>
2020-06-14 17:06:37 -0400

    DPP: Update test_src for Rule6L

--- dpp/test_src/Rule6-Minimal.c
+++ dpp/test_src/Rule6-Minimal.c
@@ -12,13 +12,32 @@ typedef struct mystruct_s {
     int (*fp)(const char*, ...);
 } mystruct_t;
 
+typedef struct mystruct_safer_s {
+  int (*fp)(const char*, ...);
+  char buffer[64];
+} mystruct_safer_t;
+
+mystruct_t g_ms = {"Hello World\n", &printf};
+mystruct_safer_t g_ms_safer = {&printf, "Hello World\n"};
+
+__attribute__((noinline))
 void func(mystruct_t *ms) {
-    gets(ms->buffer);
+    scanf("%s", ms->buffer);
     ms->fp(ms->buffer);
+}
+
+__attribute__((noinline))
+void func_safer(mystruct_safer_t *ms) {
+  scanf("%s", ms->buffer);
+  ms->fp(ms->buffer);
 }
 
 int main(void) {
     mystruct_t ms = {"Hello World\n", &printf};
+    mystruct_safer_t ms_safer = {&printf, "Hello World\n"};
     func(&ms);
+    func_safer(&ms_safer);
+    func(&g_ms);
+    func_safer(&g_ms_safer);
     return 0;
 }
